d99be3915345ad980f05d0353144913377ad9840
ÉlianePelletier <2344365@cegeplimoilou.ca>
2025-11-01 15:22:45 -0400

    interface foncitonnelle (de base)

--- client/interface.py
+++ client/interface.py
@@ -2,45 +2,31 @@ import subprocess
 
 filePath = ""
 countFiles1 = 0
-countFiles2 = 0
+bgColour = "#1b1f33"
+bgColourLight = "#3d456e"
+colour2 = "#7ccbe6"
+colour2light = "#bdeeff"
 
 
 def parcourir():
     global filePath
     global countFiles1 
-    filePath = filedialog.askopenfilename()
-    countFiles1 += 1
-    listeFichiersC.insert(countFiles1, filePath)
+    filePath = filedialog.askdirectory()
+
+    if filePath != "":
+        countFiles1 += 1
+        listeFichiersCompiles.insert(countFiles1, filePath + "/.pio/build/megaatmega2560/firmware.hex")
     
     print("parcourir")
     
-def compiler():
-    #à faire
-    #g++ file1.cpp file2.cpp -o myprogram
-    selected_indices = listeFichiersC.curselection()
-    selected_values = [listeFichiersC.get(i) for i in selected_indices]
-    command1 = "cd "
-    command2 = "g++ "
-
-    for files in selected_values:
-        command2 += files + " "
-    command2 += "-o myprogram"
-    print(command2)
-
-    try:
-        subprocess.run([command1], capture_output=True, text=True, check=True)
-        result = subprocess.run([command2], capture_output=True, text=True, check=True)
-        print("Command output:")
-        print(result.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f"Error executing command: {e}")
-        print(f"Stderr: {e.stderr}")
-
-    print("compiler")
 
 def envoyer():
     #à faire
     print("envoyer")
+
+def listboxSelect(event):
+    buttonEnvoyer.config(state = "normal")
+    buttonEnvoyer.config(bg = colour2)
 
 
 from tkinter import *
@@ -56,6 +42,7 @@ screen_height = window.winfo_screenheight()
 
 window.geometry(f"{screen_width}x{screen_height}") 
 window.title("Shacks 2025") #à changer???
+window.config(bg = bgColour)
 
 #label = Label(window,text="Hello world", font = ('Arial', 30, 'bold'), fg = 'green', bg = 'black', padx = 30, pady = 10)
 #label.place(x = 0, y = 0)
@@ -70,24 +57,47 @@ window.title("Shacks 2025") #à changer???
 #data_string.set("Hello World! But, Wait!!! You Can Select Me :)")
 #ent = Entry(window,textvariable=data_string,fg="black",bg="white",bd=0,state="readonly")
 #ent.pack()
+labelText = Label(window, text = "Veuillez choisir le dossier du projet", font = ('Calibri', 11), bg = bgColour, fg = colour2)
+labelText.place(x = 15, y = 13)
 
-labelText = Label(window, text = "Déposez votre fichier :", font = ('Calibri', 20, 'bold'))
-labelText.place(x = 15, y = 30)
+labelTextGros = Label(window, text = "Projet compilé :", font = ('Calibri', 20, 'bold'), bg = bgColour, fg = colour2)
+labelTextGros.place(x = 15, y = 35)
 
-listeFichiersC = Listbox(window, font = ('Calibri', 10), width = 90, selectmode= 'multiple')
-listeFichiersC.place(x = 15, y = 80)
+labelBorder = Label(window, bg = colour2, padx = 80, pady = 15)
+labelBorder.place(x = 22, y =257)
 
-buttonParcourir = Button(window, text = "Parcourir", command=parcourir, font = ('Calibri', 10))
+buttonParcourir = Button(window, 
+    text = "Choisir le dossier", 
+    command=parcourir, 
+    font = ('Calibri', 15, 'bold'),
+    bg = bgColour,
+    fg = colour2,
+    #activeforeground = colour2light,
+    #borderwidth = 0,
+    #highlightbackground = colour2
+    )
 buttonParcourir.place(x = 25, y = 260)
 
-buttonCompiler = Button(window, text = "Compiler", command=compiler, font = ('Calibri', 10))
-buttonCompiler.place(x = 110, y = 260)
+listeFichiersCompiles = Listbox(window, font = ('Calibri', 10), width = 130)
+listeFichiersCompiles.place(x = 15, y = 80)
+listeFichiersCompiles.bind('<<ListboxSelect>>', listboxSelect)
 
-listeFichiersCompiles = Listbox(window, font = ('Calibri', 10), width = 90)
-listeFichiersCompiles.place(x = 700, y = 80)
 
-buttonEnvoyer = Button(window, text = "Envoyer", command=envoyer, font = ('Calibri', 10))
-buttonEnvoyer.place(x = 710, y = 260)
+buttonEnvoyer = Button(window, 
+    text = "Envoyer au arduino", 
+    command=envoyer, 
+    font = ('Calibri', 15, 'bold'), 
+    state = "disabled",
+    bg = colour2light,
+    fg = bgColour,
+    activebackground = colour2light,
+    disabledforeground = bgColourLight,
+    highlightthickness = 3,
+    highlightbackground = bgColour
+    )
+buttonEnvoyer.place(x = 195, y = 260)
+
+
 
 
 
