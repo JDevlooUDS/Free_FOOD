a7c6c2d105de243501d048dced835ea5ac88c95a
DevlooJeremyUDS <devj1244@usherbrooke.ca>
2025-11-01 15:56:58 -0400

    ajout des fonction pour la gestion de signature

--- client/interface.py
+++ client/interface.py
@@ -1,8 +1,32 @@
 import subprocess
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes, serialization
 
 filePath = ""
 countFiles1 = 0
 countFiles2 = 0
+
+def generate_private_key():
+    private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+    return private_key
+
+def get_public_key(private_key):
+    return private_key.public_key()
+
+def encode_public_key(public_key):
+    pub_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    return pub_pem
+
+def sign_file(file, private_key):
+    signature = private_key.sign(
+        file,
+        padding.PKCS1v15(),
+        hashes.SHA256()
+    )
+    return signature
 
 
 def parcourir():
--- server/src/encryption.h
+++ server/src/encryption.h
@@ -1,16 +1,16 @@
 #include <wolfssl/wolfcrypt/rsa.h>
 #include <wolfssl/wolfcrypt/error-crypt.h>
 
-bool verifySignature(RsaKey *key, byte *sig, byte *hash) {
+bool verifySignature(RsaKey *key, byte *sig, word32 sigLen, byte *hash, word32 hashLen) {
 
-    byte *out = (byte*)malloc(32);
-    word32 outLen = sizeof(out);
-    if (wc_RsaSSL_Verify(sig, sizeof(sig), out, outLen, key) != 0 ) {
+    byte *out = (byte*)malloc(256);
+    word32 outLen = 256;
+    if (wc_RsaSSL_Verify(sig, sigLen, out, outLen, key) != 0 ) {
         free(out);
         return false;
     }
 
-    if (hash == out) {
+    if (memcmp(hash,out, hashLen) == 0) {
         free(out);
         return true;
     }
@@ -18,8 +18,16 @@ bool verifySignature(RsaKey *key, byte *sig, byte *hash) {
     return false;
 }
 
-int hashFile(byte *data, byte *hash) {
-    return wc_Sha256Hash(data, sizeof(data), hash);
+bool decodePubKey(RsaKey *key, byte *input, word32 inSize) {
+    word32 inOutIdx = 0;
+    if (wc_RsaPublicKeyDecode(input, &inOutIdx, key, inSize) != 0) {
+        return false;
+    }
+    return true;
+}
+
+int hashFile(byte *data, word32 dataLen, byte *hash) {
+    return wc_Sha256Hash(data, dataLen, hash);
 }
 
 
